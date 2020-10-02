d73719b8738e467765bbf38614ba5ad4d1aba541
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-02 19:13:41 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   #indexアクションはpostモデル経由でpostsテーブルから全ての情報を取得しビューへ送る 
+  #orderで新しいメモが一番上に表示される
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
   #createアクションはPostモデル経由でpostsテーブルに投稿を行う
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
