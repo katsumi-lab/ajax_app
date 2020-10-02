846767874df30fb0136d524650b04a58866337ca
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-02 19:58:33 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,16 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id]) #既読したmemoのIDが渡される
+    if post.checked  #既読であるか判定するプロパティ
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id]) #更新したレコードを取得し直し
+    render json: { post: item } #JSON形式としてchecked.jsに返却
+  end
 end
