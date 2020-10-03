1544a382d69d648a907be9f674681e0de39b21fa
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-03 11:46:24 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def create
     #メモ作成時に未読の情報を保存する
-    post = Post.create(content: params[content], checked: false)
+    post = Post.create(content: params[:content], checked: false)
     #Ajaxを実現する為にレスポンスをJSONに変更
     render json:{ post: post }
   end
